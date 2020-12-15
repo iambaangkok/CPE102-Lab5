e0fb90fa90e6a66ff61af2e04df4a8a0ff2a0c33
Baangkok Vanijyananda <iambaangkok@gmail.com>
2020-12-15 14:58:20 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
+typedef long long int lli;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    lli num = -1, even = 0, odd = 0;
+    while(num != 0){
+        cout << "Enter an integer: ";
+        cin >> num;
+        if (num == 0){
+            break;
+        }
+        if(num%2 == 0){
+            even++;
+        }else{
+            odd++;
+        }
+    }
+
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
     return 0;
 }
